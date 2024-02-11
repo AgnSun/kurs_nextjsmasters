eee51265f187b59f36558b73c02c360fd099311e
AgnSun <120824970+AgnSun@users.noreply.github.com>
2024-02-11 21:01:20 +0100

    Atomic design (#3)
    * add products and styling page
    
    * changes in styles, names of products

--- src/ui/organisms/ProductList.tsx
+++ src/ui/organisms/ProductList.tsx
@@ -3,9 +3,12 @@ import { type ProductItemType } from "@/ui/types";
 
 export const ProductList = ({ products }: { products: ProductItemType[] }) => {
 	return (
-		<ul className="grid grid-cols-1 gap-8 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+		<ul
+			className="sm-grid-cols-1 grid grid-cols-1 gap-8 sm:grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+			data-testid="products-list"
+		>
 			{products.map((product) => (
-				<ProductListItem data-testid="products-list" key={product.id} product={product} />
+				<ProductListItem key={product.id} product={product} />
 			))}
 		</ul>
 	);
