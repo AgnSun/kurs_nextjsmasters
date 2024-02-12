a8e10da330049d15277f16f303f2b42863ec9e8a
Agnieszka <agncielinska@gmail.com>
2024-02-12 20:54:58 +0100

    module 2

---
+++ src/app/products/[page]/page.tsx
@@ -0,0 +1,22 @@
+import { redirect } from "next/navigation";
+import { getProductsByPage } from "@/api/products";
+import { Pagination } from "@/ui/molecules/Pagination";
+import { ProductList } from "@/ui/organisms/ProductList";
+
+export default async function ProductsPage({ params }: { params: { page: number } }) {
+	const products = await getProductsByPage(params.page);
+	const allProducts = 100;
+	const productsPerPage = 20;
+	const totalPages = Math.ceil(allProducts / productsPerPage);
+
+	if (!params.page || isNaN(Number(params.page)) || Number(params.page) <= 0) {
+		return redirect("/products/1");
+	}
+
+	return (
+		<div>
+			<Pagination totalPages={totalPages} />
+			<ProductList products={products} />;
+		</div>
+	);
+}
---
+++ src/ui/molecules/Pagination.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { ActiveLink } from "@/ui/atoms/ActiveLink";
+
+export const Pagination = ({ totalPages }: { totalPages: number }) => {
+	const pages = Array.from({ length: totalPages }, (_, index) => index + 1);
+	return (
+		<nav>
+			<ul aria-label="pagination" className="mt-2 flex justify-center space-x-4 pb-4">
+				{pages.map((page) => {
+					return (
+						<li key={page}>
+							<ActiveLink href={`/products/${page}`} activeClassName="underline" match>
+								{page}
+							</ActiveLink>
+						</li>
+					);
+				})}
+			</ul>
+		</nav>
+	);
+};
