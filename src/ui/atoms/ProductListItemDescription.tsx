dde1c4fffb666053281d22f746325b2a540a365e
AgnSun <120824970+AgnSun@users.noreply.github.com>
2024-02-26 22:06:42 +0100

    fix (#5)

--- src/ui/atoms/ProductListItemDescription.tsx
+++ src/ui/atoms/ProductListItemDescription.tsx
@@ -6,12 +6,11 @@ type ProductListItemDescriptionProps = {
 };
 
 export const ProductListItemDescription = ({
-	product: { name, category, price },
+	product: { category, price },
 }: ProductListItemDescriptionProps) => {
 	return (
 		<div className="mt-2 flex justify-between">
 			<div>
-				<h1 className="text-sm font-semibold text-gray-700">{name}</h1>
 				<p className="text-sm text-gray-500">
 					<span className="sr-only">Kategoria: </span>
 					{category}
