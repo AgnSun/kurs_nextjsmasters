b4ee77261d585f6890ea861ee787786957521b65
AgnSun <120824970+AgnSun@users.noreply.github.com>
2024-03-09 23:22:21 +0100

    changes (#17)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Collections } from "@/ui/organisms/Collections";
 import { SuggestedProductsList } from "@/ui/organisms/SuggestedProducts";
 
 export default function HomePage() {
 	return (
-		<div data-testid="related-products">
-			<SuggestedProductsList />
-		</div>
+		<>
+			<Collections />
+			<div data-testid="related-products">
+				<SuggestedProductsList />
+			</div>
+		</>
 	);
 }
