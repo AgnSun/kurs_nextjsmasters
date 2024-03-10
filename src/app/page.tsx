f0dc8bbcce0009a34e7fa8615dd0b2a07789f12a
AgnSun <120824970+AgnSun@users.noreply.github.com>
2024-03-10 10:46:30 +0100

    module3-fix9 (#20)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Collections } from "@/ui/organisms/Collections";
+// import { Collections } from "@/ui/organisms/Collections";
 import { SuggestedProductsList } from "@/ui/organisms/SuggestedProducts";
 
 export default function HomePage() {
 	return (
 		<>
-			<Collections />
+			{/* <Collections /> */}
 			<div data-testid="related-products">
 				<SuggestedProductsList />
 			</div>
--- src/ui/atoms/ProductListItemDescription.tsx
+++ src/ui/atoms/ProductListItemDescription.tsx
@@ -12,10 +12,10 @@ export const ProductListItemDescription = ({
 		<>
 			<div className="mt-2 flex justify-between">
 				<div>
-					<p className="text-lg font-medium text-gray-900">
+					<h1 className="text-lg font-medium text-gray-900">
 						<span className="sr-only">Nazwa: </span>
 						{name}
-					</p>
+					</h1>
 				</div>
 				<p className="text-lg font-bold  text-gray-900">
 					<span className="sr-only">Cena: </span>
