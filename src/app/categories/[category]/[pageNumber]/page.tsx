064fb62eb88ba31b5d01bc4d1e7c4b3d5874bd5d
AgnSun <120824970+AgnSun@users.noreply.github.com>
2024-03-17 18:40:55 +0100

    fix (#28)

--- src/app/categories/[category]/[pageNumber]/page.tsx
+++ src/app/categories/[category]/[pageNumber]/page.tsx
@@ -4,13 +4,13 @@ import { ProductsGetByCategoryDocument } from "@/gql/graphql";
 import { executeGraphql } from "@/api/graphqlApi";
 import { PaginationForCategory } from "@/ui/molecules/PaginationForCategory";
 
-export const generateStaticParams = async ({ params }: { params: { category: string } }) => {
-	if (params.category === "t-shirts") {
-		return [{ pageNumber: "1" }, { pageNumber: "2" }];
-	} else {
-		return [{ pageNumber: "1" }];
-	}
-};
+// export const generateStaticParams = async ({ params }: { params: { category: string } }) => {
+// 	if (params.category === "t-shirts") {
+// 		return [{ pageNumber: "1" }, { pageNumber: "2" }];
+// 	} else {
+// 		return [{ pageNumber: "1" }];
+// 	}
+// };
 
 export const generateMetadata = () => {
 	return {
